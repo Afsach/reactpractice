9ef5c73b4ab134aaf586e55d868e23059f1f50b0
afsarjahan ansari <nagma1804@gmail.com>
2023-11-21 11:38:37 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css'
+
+import  {UseEffectPractice}  from './Components/UseEffectPractice';
 
 const App = () => {
      
     return (
         <>
         practice
+        <UseEffectPractice/>
         </>
     )
 }
---
+++ src/Components/UseEffectPractice.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const UseEffectPractice =()=>{
+    return (
+        <>
+        <h1> Code snippetted</h1>
+       
+        </>
+    )
+}
